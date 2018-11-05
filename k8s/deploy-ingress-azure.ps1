b3d74ff09206b2bcedd9952aabb3a11d6162911d
Jim Priestley <japriest@microsoft.com>
2018-11-05 17:02:51 -0500

    reverted ingress items back to original code works as written if AKS cluster is deployed with --disable-rbac flag

--- k8s/deploy-ingress-azure.ps1
+++ k8s/deploy-ingress-azure.ps1
@@ -1,8 +1,3 @@
-﻿#These two lines added to resolve Azure AKS now requiring RBAC on NGINX by default
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/cloud-generic.yaml
-
-
-kubectl patch deployment -n ingress-nginx nginx-ingress-controller --type=json --patch="$(cat nginx-ingress\publish-service-patch.yaml)"
+﻿kubectl patch deployment -n ingress-nginx nginx-ingress-controller --type=json --patch="$(cat nginx-ingress\publish-service-patch.yaml)"
 kubectl apply -f nginx-ingress\azure\service.yaml
 kubectl apply -f nginx-ingress\patch-service-without-rbac.yaml
--- k8s/deploy-ingress.ps1
+++ k8s/deploy-ingress.ps1
@@ -8,5 +8,10 @@ kubectl apply -f nginx-ingress\tcp-services-configmap.yaml
 kubectl apply -f nginx-ingress\udp-services-configmap.yaml
 kubectl apply -f nginx-ingress\without-rbac.yaml
 
+#These two lines added to resolve Azure AKS now requiring RBAC on NGINX by default
+#kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
+#kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/cloud-generic.yaml
+
+
 
 
