67737b6fb1536ee7d8b4b1d5cd80ffcbda2b11e5
Jim Priestley <japriest@microsoft.com>
2018-11-13 17:12:22 -0500

    added publish ProductPriceChanged

---
+++ UpdateItemPriceInEast/UpdateItemPriceInEast/ProductPriceChangedIntegrationEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpdateItemPriceInEast
+{
+    public class ProductPriceChangedIntegrationEvent
+    {
+        public Guid Id { get; }
+        public DateTime CreationDate { get; }
+        public int ProductId { get; private set; }
+        public decimal NewPrice { get; private set; }
+        public decimal OldPrice { get; private set; }
+
+        public ProductPriceChangedIntegrationEvent(int productId, decimal newPrice, decimal oldPrice)
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.UtcNow;
+            ProductId = productId;
+            NewPrice = newPrice;
+            OldPrice = oldPrice;
+        }
+    }
+}
---
+++ UpdateItemPriceInEast/UpdateItemPriceInEast/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+
+namespace UpdateItemPriceInEast
+{
+    //Write 2 East 
+
+
+
+    class Program
+    {
+        const string TopicName = "eshop_event_bus";
+        const string ServiceBusWriteConnectionString = "Endpoint=sb://eshopsbcdcbguvwwotha.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=rD6FwEQDuXe6ZutG942tHrhBYohenPknDiu7H0mxi/k=";
+
+        static ITopicClient topicClient;
+
+        static void Main(string[] args)
+        {
+
+            topicClient = new TopicClient(ServiceBusWriteConnectionString, TopicName);
+
+            var @event = new ProductPriceChangedIntegrationEvent(Convert.ToInt32(args[0]),Convert.ToDecimal(args[1]),0);
+
+            var jsonMessage = JsonConvert.SerializeObject(@event);
+            var body = Encoding.UTF8.GetBytes(jsonMessage);
+
+            var message = new Message
+            {
+                MessageId = Guid.NewGuid().ToString(),
+                Body = body,
+                Label = "ProductPriceChanged",
+            };
+
+            topicClient.SendAsync(message);
+        }
+    }
+}
